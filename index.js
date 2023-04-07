5116fc8ac6913a72ffcd07b83859a3ece11d05ea
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-04-07 09:41:57 -0400

    trying to append searched player image

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
-// const btn = document.querySelector('button');
-// btn.onclick = searchPlayer;
 
-// async function searchPlayer() {
-//   const playerName = document.getElementById('playerName').value;
-//   const apiUrl = 'https://www.balldontlie.io/api/v1/players?search=' + playerName;
-
-//   try {
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     const playerId = data.data[0].id;
-//     const statsApiUrl =
-//       'https://www.balldontlie.io/api/v1/season_averages?player_ids[]=' + playerId;
-
-//     const statsResponse = await fetch(statsApiUrl);
-//     const statsData = await statsResponse.json();
-//     const playerStats = statsData.data[0];
-
-
-
-//     const playerFullName = data.data[0].first_name + ' ' + data.data[0].last_name;
-//     const playerStatsDiv = document.getElementById('playerStats');
-//     playerStatsDiv.innerHTML =
-//         '<h2>' + playerFullName + '</h2>' +
-//         '<p>Points per game: ' + playerStats.pts + '</p>' +
-//         '<p>Assists per game: ' + playerStats.ast + '</p>' +
-//         '<p>Rebounds per game: ' + playerStats.reb + '</p>' +
-//         '<p>Field goal percentage: ' + playerStats.fg_pct + '</p>';
-        
-
-//     document.getElementById('playerName').value = '';
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// }
 const btn = document.querySelector('button');
 btn.onclick = searchPlayer;
 
